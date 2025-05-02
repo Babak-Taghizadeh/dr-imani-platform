386bfd7ea20626fc0e09a38718fe3af45ac7cfdc
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-02 21:56:01 +0330

    feat: add navigations static data

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const NAV_ITEMS: { title: string; path: string }[] = [
+  {
+    title: "خانه",
+    path: "/",
+  },
+  {
+    title: "درباره من",
+    path: "/about",
+  },
+  {
+    title: "تماس با ما",
+    path: "/contact",
+  },
+  {
+    title: "وبلاگ",
+    path: "/blogs",
+  },
+];
