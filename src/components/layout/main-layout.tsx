258d53372e7cf69d77e33138bc5274dde7faad9d
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-02 21:57:14 +0330

    feat: implement layout for pages

---
+++ src/components/layout/main-layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "./footer";
+import Header from "./header";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
