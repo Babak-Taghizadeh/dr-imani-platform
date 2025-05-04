327f609fc9ed111209a86571a473a62e6406a864
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-04 13:34:40 +0330

    style: modify styles

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import MobileHeader from "./header/mobile-header";
 import DesktopHeader from "./header/desktop-header";
+import MobileHeader from "./header/mobile-header";
 
 const Header = () => {
   return (
-    <header className="bg-foreground text-background flex h-16 w-full items-center justify-between border-b px-4 shadow-sm md:px-10">
+    <header className="bg-foreground text-background flex h-16 w-full items-center border-b justify-between px-4 md:px-10">
       <MobileHeader />
       <DesktopHeader />
       <Link href="/" className="text-xl font-bold">
