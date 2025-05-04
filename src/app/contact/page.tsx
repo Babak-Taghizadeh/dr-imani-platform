1ef7f7e86793cb3092a6f6adb41efbf591ca9d7d
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-04 14:03:33 +0330

    feat: add & apply map component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import NeshanMap from "@/components/sections/map";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <NeshanMap />
+    </div>
+  );
+};
+
+export default Contact;
