8ec3071de6422f2dd6e182a680067186c6891e34
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-04 13:35:08 +0330

    feat: apply components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+import Faq from "@/components/sections/faq";
+import Hero from "@/components/sections/hero";
+import ServicesSummary from "@/components/sections/services-summary";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Button>Button</Button>
-      </main>
-    </div>
+    <>
+      <Hero />
+      <ServicesSummary />
+      <Faq />
+    </>
   );
 }
