f31d6ad5474c6b6bcf14494527e383d0ceb95c79
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-07 12:35:40 +0330

    feat: add admin layout's header

---
+++ src/components/sections/admin/admin-header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddBlog from "./add-blog";
+
+const AdminHeader = () => {
+  return (
+    <div className="flex h-16 w-1/2 items-center justify-between">
+      <h1 className="text-2xl font-bold">پنل مدیریت</h1>
+      <AddBlog />
+    </div>
+  );
+};
+
+export default AdminHeader;
