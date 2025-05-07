bc84514895965ba2e7f3e7ed818d892dea93b73d
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-07 12:33:45 +0330

    chore: add nextauth configuration

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const isAdmin = token?.name === "Admin";
+
+  const isProtectedRoute = req.nextUrl.pathname.startsWith("/admin");
+
+  if (isProtectedRoute && !isAdmin) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
