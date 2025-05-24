a1d5ec9f3b47ab68d59e4647576bde2b7c3b5556
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-24 11:39:59 +0330

    feat: add loading handling

---
+++ src/app/(main)/blogs/loading.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="text-muted-foreground mt-24 w-full text-center">
+      در حال بارگذاری بلاگ‌ها...
+    </div>
+  );
+};
+
+export default Loading;
