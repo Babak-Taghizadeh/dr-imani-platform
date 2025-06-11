79498629b9cd78f214c984edf501c7949fe2f410
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-11 14:53:17 +0330

    refactor: adjust card positioning and scaling in getCardStyle for improved layout

--- src/utils/get-card-style.ts
+++ src/utils/get-card-style.ts
@@ -7,11 +7,11 @@ const getCardStyle = (
 
   switch (position) {
     case 0:
-      return { x: 0, y: -30, scale: 1, opacity: 1, zIndex: 30 };
+      return { x: 0, y: -20, scale: 1, opacity: 1, zIndex: 30 };
     case 1:
-      return { x: 170, scale: 0.9, opacity: 0.5, zIndex: 20 };
+      return { x: 200, scale: 0.9, opacity: 0.5, zIndex: 20 };
     case totalProjects - 1:
-      return { x: -170, scale: 0.9, opacity: 0.5, zIndex: 20 };
+      return { x: -200, scale: 0.9, opacity: 0.5, zIndex: 20 };
     default:
       return null;
   }
