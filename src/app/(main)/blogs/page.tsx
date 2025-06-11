6427de89d00ab09c7ae034b759b64eae6d46c0ef
Babak Taghizadeh <babak.tz98@gmail.com>
2025-06-11 14:52:44 +0330

    refactor: replace Loading component with BlogsLoader for improved loading experience

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -1,17 +1,25 @@
 import BlogCard from "@/components/sections/blog-card";
+import BlogsLoader from "@/components/sections/blogs-loader";
+import SectionHeader from "@/components/shared/section-header";
 import getBlogs from "@/utils/get-blogs";
+import { Suspense } from "react";
 
 const BlogsPage = async () => {
   const blogs = await getBlogs();
 
   return (
-    <div className="bg-foreground grid grid-cols-1 gap-6 p-12 sm:grid-cols-2 lg:grid-cols-3">
-      <h1 className="text-background col-span-full text-xl font-bold md:text-3xl">
-        وبلاگ
-      </h1>
-      {blogs.map((blog, index) => (
-        blog.status === "منتشر شده" && <BlogCard blog={blog} key={index} />
-      ))}
+    <div className="bg-foreground grid grid-cols-1 gap-6 px-8 py-12 sm:grid-cols-2 lg:grid-cols-3">
+      <SectionHeader
+        title="وبلاگ و مقالات"
+        className="col-span-full"
+        theme="dark"
+      />
+      <Suspense fallback={<BlogsLoader />}>
+        {blogs.map(
+          (blog, index) =>
+            blog.status === "منتشر شده" && <BlogCard blog={blog} key={index} />,
+        )}
+      </Suspense>
     </div>
   );
 };
