bb4589825fc8c3f65dd266c401628dda5ac62fdc
Babak Taghizadeh <babak.tz98@gmail.com>
2025-07-26 11:40:51 +0330

    style: modify blog card UI

--- src/components/sections/blogs/blog-card.tsx
+++ src/components/sections/blogs/blog-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -13,9 +15,9 @@ import { ArrowLeft } from "lucide-react";
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
   return (
-    <Card className="bg-secondary text-foreground py-4">
-      <CardHeader className="px-4">
-        <div className="relative h-64 w-full">
+    <Card className="bg-card/80 hover:bg-card/90 border-border/50 group overflow-hidden transition-all hover:shadow-md">
+      <CardHeader className="relative">
+        <div className="relative h-48 w-full overflow-hidden rounded-2xl lg:h-64">
           <Image
             src={blog.img}
             alt={blog.title}
@@ -23,21 +25,34 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             quality={80}
             loading="lazy"
-            className="rounded-md object-cover transition-all duration-300 hover:scale-105"
+            className="object-cover transition-all duration-500 group-hover:scale-105"
           />
         </div>
+        <div className="bg-card/90 text-accent-foreground/80 absolute top-4 right-8 rounded-full px-3 py-1 text-xs backdrop-blur-sm">
+          <span className="bg-chart-4/50 ml-1 inline-block h-2 w-2 animate-pulse rounded-full" />
+          {new Date(blog.createdAt).toLocaleDateString("fa-IR")}
+        </div>
       </CardHeader>
-      <CardContent className="-mt-4">
-        <CardTitle className="mb-2 lg:text-lg">{blog.title}</CardTitle>
-        <CardDescription>{blog.excerpt}</CardDescription>
+
+      <CardContent>
+        <CardTitle className="text-primary/90 group-hover:text-primary mb-2 transition-colors lg:text-xl">
+          {blog.title}
+        </CardTitle>
+        <CardDescription className="line-clamp-3 leading-relaxed">
+          {blog.excerpt}
+        </CardDescription>
       </CardContent>
-      <CardFooter className="mt-auto mr-auto">
+
+      <CardFooter className="justify-end">
         <Link
-          className="text-primary flex items-center gap-1 hover:underline"
+          className="text-primary flex items-center gap-1 text-sm transition-all hover:underline hover:underline-offset-4"
           href={`/blogs/${blog.id}`}
         >
-          ادامه مطلب
-          <ArrowLeft size={15} />
+          <span>ادامه مطلب</span>
+          <ArrowLeft
+            size={15}
+            className="transition-transform group-hover:-translate-x-1"
+          />
         </Link>
       </CardFooter>
     </Card>
