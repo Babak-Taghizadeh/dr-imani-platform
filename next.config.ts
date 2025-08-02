17657c43ea4c83ae0851d80cc67842673efe1eba
Babak Taghizadeh <babak.tz98@gmail.com>
2025-08-02 04:14:27 +0330

    chore: update next config to fix post build errors

--- next.config.ts
+++ next.config.ts
@@ -22,21 +22,20 @@ const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value: `
-    default-src 'self';
-    base-uri 'self';
-    form-action 'self';
-    script-src 'self' https://static.neshan.org https://unpkg.com;
-    script-src-attr 'none';
-    style-src 'self' https://unpkg.com 'unsafe-inline';
-    style-src-attr 'none';
-    img-src 'self' blob: data: https://*.neshan.org https://*.tiles.mapbox.com;
-    font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' https://static.neshan.org https://vts.neshan.org https://api.neshan.org;
-    worker-src 'self' blob:;
-    child-src 'self' blob:;
-    frame-src 'self' https://*.neshan.org;
-  `
-      .replace(/\n\s+/g, " ")
+      default-src 'self';
+      base-uri 'self';
+      form-action 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.neshan.org https://unpkg.com;
+      script-src-elem 'self' 'unsafe-inline' https://static.neshan.org https://unpkg.com;
+      style-src 'self' 'unsafe-inline' https://unpkg.com;
+      style-src-attr 'self' 'unsafe-inline';
+      img-src 'self' blob: data: https://*.neshan.org https://*.tiles.mapbox.com;
+      font-src 'self' https://fonts.gstatic.com;
+      connect-src 'self' https://static.neshan.org https://vts.neshan.org https://api.neshan.org;
+      worker-src 'self' blob:;
+      frame-src 'self' https://*.neshan.org;
+    `
+      .replace(/\s+/g, " ")
       .trim(),
   },
 ];
