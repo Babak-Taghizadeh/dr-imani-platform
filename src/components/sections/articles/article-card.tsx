d4502ce2ad1ba66921448cc93629477f19d7eacc
Babak Taghizadeh <babak.tz98@gmail.com>
2025-12-10 21:03:03 +0330

    style: modify styles to fix overflow issues.

--- src/components/sections/articles/article-card.tsx
+++ src/components/sections/articles/article-card.tsx
@@ -31,22 +31,23 @@ const ArticleCard = ({ article, index }: ArticleCardProps) => {
   const mobileAnimation = createViewportAnimation();
   return (
     <motion.div {...(isDesktop ? desktopAnimation : mobileAnimation)}>
-      <Card className="bg-card/80 hover:bg-card/90 border-border/50 group transition-all hover:shadow-md">
+      <Card className="bg-card/80 hover:bg-card/90 border-border/50 group gap-4 transition-all hover:shadow-md">
         <CardHeader className="relative">
-          <CardTitle className="text-primary/90 group-hover:text-primary transition-colors md:text-xl">
+          <CardTitle
+            dir="ltr"
+            className="text-primary/90 group-hover:text-primary mr-auto line-clamp-1 max-w-[70%] leading-6 text-ellipsis transition-colors md:text-lg"
+          >
             {article.title}
           </CardTitle>
-          <CardDescription className="text-accent-foreground/80 flex items-center justify-end absolute left-6 -top-3 mt-2 gap-1 text-xs">
+          <CardDescription className="text-accent-foreground/80 absolute top-3 right-6 mt-2 flex items-center justify-end gap-1 text-xs">
             <span className="bg-chart-1/50 h-2 w-2 animate-pulse rounded-full" />
             تاریخ انتشار:{" "}
             {new Date(article.publishedAt).toLocaleDateString("fa-IR")}
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="relative flex flex-col gap-3 pt-0 text-right">
-          <div className="via-border/30 absolute top-0 left-4 h-full w-px bg-gradient-to-b from-transparent to-transparent" />
-
-          <CardDescription className="line-clamp-3 leading-relaxed">
+        <CardContent className="relative flex h-12 flex-col gap-3 pt-0 text-right">
+          <CardDescription className="line-clamp-2 leading-relaxed">
             {article.summary}
           </CardDescription>
         </CardContent>
@@ -54,11 +55,7 @@ const ArticleCard = ({ article, index }: ArticleCardProps) => {
         <CardFooter className="justify-end">
           {article.inputType === "file" && article.fileUrl ? (
             <Link href={article.fileUrl} download>
-              <Button
-                size="sm"
-                variant="outline"
-                className="!border-primary"
-              >
+              <Button size="sm" variant="outline" className="!border-primary">
                 <Download className="ml-2 h-4 w-4 opacity-80 group-hover:opacity-100" />
                 <span>دانلود مقاله</span>
               </Button>
@@ -69,11 +66,7 @@ const ArticleCard = ({ article, index }: ArticleCardProps) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Button
-                size="sm"
-                variant="outline"
-                className="!border-primary"
-              >
+              <Button size="sm" variant="outline" className="!border-primary">
                 <ExternalLink className="ml-2 h-4 w-4 opacity-80 group-hover:opacity-100" />
                 <span>مشاهده در گوگل اسکالر</span>
               </Button>
