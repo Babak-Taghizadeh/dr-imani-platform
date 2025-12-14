0bdf81918a346997d5acd1201b9219bbeb9b7350
Babak Taghizadeh <babak.tz98@gmail.com>
2025-12-14 14:00:44 +0330

    refactor: refactor admin layout by removing local font and simplifying structure

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../(main)/globals.css";
 import { Toaster } from "sonner";
-
-const IRANSansXV = localFont({
-  src: "../../../public/fonts/IRANSansXV.woff2",
-});
 
 export const metadata: Metadata = {
   title: {
@@ -41,11 +36,9 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl">
-      <body className={`${IRANSansXV.className} antialiased`}>
-        {children}
-        <Toaster expand={true} position="top-center" richColors />
-      </body>
-    </html>
+    <>
+      {children}
+      <Toaster expand={true} position="top-center" richColors />
+    </>
   );
 }
