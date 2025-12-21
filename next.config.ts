42f5add5a1595dec03e5c82df69d4419eddf63c4
Babak Taghizadeh <babak.tz98@gmail.com>
2025-12-21 16:19:27 +0330

    fix: add trustseal domain to img-src in security headers for enhanced content security

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ const securityHeaders = [
       script-src-elem 'self' 'unsafe-inline' https://static.neshan.org https://unpkg.com https://www.googletagmanager.com;
       style-src 'self' 'unsafe-inline' https://unpkg.com;
       style-src-attr 'self' 'unsafe-inline';
-      img-src 'self' blob: data: https://*.neshan.org https://*.tiles.mapbox.com;
+      img-src 'self' blob: data: https://*.neshan.org https://*.tiles.mapbox.com https://trustseal.enamad.ir;
       font-src 'self' https://fonts.gstatic.com;
       connect-src 'self' https://static.neshan.org https://vts.neshan.org https://api.neshan.org https://www.google-analytics.com https://www.googletagmanager.com;
       worker-src 'self' blob:;
