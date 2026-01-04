8579a2e5f2d642e1a3a44c7e0d6798b875b0236a
Babak Taghizadeh <babak.tz98@gmail.com>
2026-01-04 22:57:00 +0330

    fix: update security headers in next.config.ts to enhance content security policy and improve site security

--- next.config.ts
+++ next.config.ts
@@ -24,16 +24,15 @@ const securityHeaders = [
     value: `
       default-src 'self';
       base-uri 'self';
-      form-action 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.neshan.org https://unpkg.com https://www.googletagmanager.com;
-      script-src-elem 'self' 'unsafe-inline' https://static.neshan.org https://unpkg.com https://www.googletagmanager.com;
+      form-action 'self' https://sep.shaparak.ir;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://www.googletagmanager.com;
+      script-src-elem 'self' 'unsafe-inline' https://unpkg.com https://www.googletagmanager.com;
       style-src 'self' 'unsafe-inline' https://unpkg.com;
       style-src-attr 'self' 'unsafe-inline';
-      img-src 'self' blob: data: https://*.neshan.org https://*.tiles.mapbox.com https://trustseal.enamad.ir;
+      img-src 'self' blob: data: https://trustseal.enamad.ir;
       font-src 'self' https://fonts.gstatic.com;
-      connect-src 'self' https://static.neshan.org https://vts.neshan.org https://api.neshan.org https://www.google-analytics.com https://www.googletagmanager.com;
+      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com;
       worker-src 'self' blob:;
-      frame-src 'self' https://*.neshan.org;
     `
       .replace(/\s+/g, " ")
       .trim(),
