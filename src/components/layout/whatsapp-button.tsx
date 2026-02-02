3b9ea558894a27aa7ae9c97d265499155bd5acff
Babak Taghizadeh <babak.tz98@gmail.com>
2026-02-02 18:27:01 +0330

    refactor: simplify contact Button logic.

--- src/components/layout/whatsapp-button.tsx
+++ src/components/layout/whatsapp-button.tsx
@@ -4,21 +4,18 @@ import { buttonVariants } from "../ui/button";
 import { cn } from "@/lib/utils";
 import WhatsAppIcon from "../../../public/icons/whatsapp.svg";
 
-interface WhatsAppButtonProps {
-  showText?: boolean;
-}
-
-export function WhatsAppButton({ showText = true }: WhatsAppButtonProps) {
+export function WhatsAppButton() {
   return (
     <Link
       href="https://wa.me/989147360827"
       className={cn(
         buttonVariants(),
-        "group relative flex items-center gap-1.5 rounded-full bg-[#25D366] p-2 hover:bg-[#25D366]/90",
+        "relative flex items-center gap-1.5 bg-[#25D366] p-2 hover:bg-[#25D366]/80",
       )}
       rel="noopener noreferrer"
       target="_blank"
     >
+      <span className="font-bold">واتساپ</span>
       <Image
         src={WhatsAppIcon}
         alt="WhatsApp Icon"
@@ -28,13 +25,8 @@ export function WhatsAppButton({ showText = true }: WhatsAppButtonProps) {
         className="brightness-0 invert"
         aria-hidden="true"
       />
-      {showText && (
-        <span className="hidden font-semibold [@media(min-width:940px)]:inline-block">
-          واتساپ
-        </span>
-      )}
+
       <span className="absolute -top-1 -right-1 h-2 w-2 animate-pulse rounded-full bg-white/80"></span>
     </Link>
   );
 }
-
