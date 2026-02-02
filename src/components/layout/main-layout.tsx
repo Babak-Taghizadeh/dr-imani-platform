90cdb9d271fee2b16af6c367ded7dbfcf19c8839
Babak Taghizadeh <babak.tz98@gmail.com>
2026-02-02 18:28:25 +0330

    feat: add asap contact button to layout,.

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -2,6 +2,7 @@ import Footer from "./footer";
 import Header from "./header";
 import { Providers } from "../providers";
 import { Toaster } from "@/components/ui/sonner";
+import { FloatingContactButton } from "./floating-contact-button";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -15,6 +16,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
         <main className="flex-1">{children}</main>
         <Footer />
       </div>
+      <FloatingContactButton />
       <Toaster expand={true} position="top-center" richColors />
     </Providers>
   );
